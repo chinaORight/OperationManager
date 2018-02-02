719fc5965e5c4d1756deac74143d9bc2cce414ef
chinaORight <785754840@qq.com>
2018-02-02 15:26:35 +0800

    Add files via upload

---
+++ OperationManager.Common/TableSource.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace OperationManager.Common
+{
+    public class TableSource<T> where T : class, new()
+    {
+        public long Total { get; set; }
+
+        public List<T> Rows { get; set; }
+    }
+}
