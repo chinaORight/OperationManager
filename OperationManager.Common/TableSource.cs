73665a159ef43d634c3fec03998a14f69cda1e24
chinaORight <785754840@qq.com>
2020-04-18 00:20:54 +0800

    # ------------------------ >8 dadasdada------------------------ # Do not touch the line above. # Everything below will be removed. # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up-to-date with 'origin/master'. # Changes to be committed: # # 	modified:   OperationManager.Common/TableSource.cs

--- OperationManager.Common/TableSource.cs
+++ OperationManager.Common/TableSource.cs
@@ -4,8 +4,8 @@ namespace OperationManager.Common
 {
     public class TableSource<T> where T : class, new()
     {
-        public long Total { get; set; }
+        public long Total  { get; set; }
 
-        public List<T> Rows { get; set; }
+        public List<T> Rows { get; set; } 
     }
 }
